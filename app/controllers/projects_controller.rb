ae2caa550d0af238f73f653979986b51ddcd47af
Reed Nelson-Saunders <reedns@hotmail.com>
2015-01-09 13:15:42 -0800

    add edit and update projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,8 +13,23 @@ class ProjectsController < ApplicationController
       redirect_to projects_path, notice: 'Success! New project created.'
     else
       @project = Project.new
-      flash[:error] = 'Oops! Please fix errors below.'
+      flash[:error] = 'Oops! Please fix the errors below.'
       render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    project = Project.find(params[:id])
+    if project.update(project_params)
+      redirect_to projects_path, notice: 'Success! Project updated.'
+    else
+      flash[:error] = 'Oops! Please fix the errors below.'
+      @project = project
+      render :edit
     end
   end
 
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -15,7 +15,7 @@ class ProjectsControllerTest < ActionController::TestCase
     assert assigns(:project)
   end
 
-  test 'with valid attributes #create should create a project' do
+  test 'valid #create' do
     assert_difference('Project.count') do
       post :create,  project: attributes_for(:project)
     end
@@ -24,12 +24,44 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_equal 'Success! New project created.', flash[:notice]
   end
 
-  test 'without valid attributes #create should not create a new project' do
+  test 'invalid #create' do
     assert_no_difference('Project.count') do
       post :create, project: attributes_for(:invalid_project)
     end
 
     assert_template :new
-    assert_equal 'Oops! Please fix errors below.', flash[:error]
+    assert_equal 'Oops! Please fix the errors below.', flash[:error]
+  end
+
+  test '#edit' do
+    project = create(:project)
+
+    get :edit, id: project.id
+    assert_response :success
+    assert assigns(:project)
+  end
+
+  test 'valid #update' do
+    project = create(:project)
+
+    patch :update, id: project.id, project: { name: 'Best Project', tech: 'Rails'}
+    assert_equal 'Best Project', project.reload.name
+    assert_equal 'Rails', project.reload.tech
+    assert_redirected_to projects_path
+    assert_equal 'Success! Project updated.', flash[:notice]
+  end
+
+  test 'invalid #update' do
+    project = create(:project)
+
+    patch :update, id: project.id, project: { name: nil}
+    assert_equal project.name, project.reload.name
+    assert_template :edit
+    assert_equal 'Oops! Please fix the errors below.', flash[:error]
   end
 end
+
+
+
+
+
